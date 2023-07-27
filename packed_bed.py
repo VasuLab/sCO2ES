dac7f233c03bfb8716c8a95e0fec10ebe06f7a31
Cory Kinney <cory.kinney@ucf.edu>
2023-07-27 10:52:05 -0400

    Separated out radiative heat transfer coefficients and film thickness ratio

--- packed_bed.py
+++ packed_bed.py
@@ -1,13 +1,95 @@
 import numpy as np
 
 
+def effective_film_thickness_ratio(k_s: float, k_f: float, eps: float):
+    r"""
+    Calculates the ratio between the effective thickness of the fluid film adjacent to the surface of two solid
+    particles and the particle diameter according to the interpolation of Kunii and Smith[^1] for
+    $0.260 \leq \varepsilon \leq 0.476$
+
+    $$
+    \phi = \phi_2 + (\phi_1 - \phi_2) \frac{\varepsilon - 0.260}{0.216}
+    $$
+
+    where
+
+    $$
+    \phi_i = \frac{1}{2} \frac{\left(\frac{\kappa-1}{\kappa}\right)^2 \sin^2{\theta_i}} {\ln\left[\kappa -
+    (\kappa - 1)\cos{\theta_i}\right] - \left(\frac{\kappa - 1}{\kappa}\right) (1 - \cos{\theta_i)}} - \frac{2}{3\kappa}
+    $$
+
+    and $\sin^2 \theta_i = \frac{1}{n_i}$ for $n_1 = 1.5$ and $n_2 = 4\sqrt{3}$.
+
+    [1] D. Kunii and J. Smith, “Heat transfer characteristics of porous rocks,” AIChE Journal, vol. 6, no. 1, pp.
+    71–78, 1960.
+
+    Parameters:
+        k_s: Thermal conductivity of the solid [W/m K].
+        k_f: Thermal conductivity of the fluid [W/m K].
+        eps: Void fraction [-].
+
+    """
+    kappa = k_s / k_f
+
+    def phi_i(n_i):
+        cos_theta_i = (1 - 1 / n_i) ** 0.5
+        return 1 / 2 * ((kappa - 1) / kappa) ** 2 / n_i / (
+                np.log(kappa - (kappa - 1) * cos_theta_i) - (kappa - 1) / kappa * (1 - cos_theta_i)
+        ) - 2 / (3 * kappa)
+
+    phi1 = phi_i(1.5)
+    phi2 = phi_i(4 * 3**0.5)
+    return phi1 + (phi2 - phi1) * (eps - 0.26) / 0.216
+
+
+def void_radiative_heat_transfer_coeff(T: float, eps: float, E_s: float):
+    r"""
+    Calculates the void-to-void radiative heat transfer coefficient for a packed bed given by the correlation of Yagi
+    and Kunii[^1]
+
+    $$
+    h_{rv} = \frac{0.1952}{1 + \frac{\varepsilon (1-E_s)}{2E_s (1-\varepsilon)}} \left(\frac{T}{100}\right)^3
+    $$
+
+    [^1]: S. Yagi and D. Kunii, “Studies on effective thermal conductivities in packed beds,” AIChE Journal,
+    vol. 3, no. 3, pp. 373–381, 1957.
+
+    Parameters:
+        T: Temperature [K].
+        eps: Void fraction [-].
+        E_s: Emissivity of the solid [-].
+
+    """
+    return 0.1952 * (T/100)**3 / (1 + eps * (1 - E_s) / (2 * E_s * (1 - eps)))
+
+
+def surface_radiative_heat_transfer_coeff(T: float, E_s: float):
+    r"""
+    Calculates the surface-to-surface radiative heat transfer coefficient for a packed bed given by the correlation of
+    Yagi and Kunii[^1]
+
+    $$
+    h_{rs} = \frac{0.1952E_s}{2-E_s} \left( \frac{T}{100}\right)^3
+    $$
+
+    [^1]: S. Yagi and D. Kunii, “Studies on effective thermal conductivities in packed beds,” AIChE Journal,
+    vol. 3, no. 3, pp. 373–381, 1957.
+
+    Parameters:
+        T: Temperature [K].
+        E_s: Emissivity of the solid [-].
+
+    """
+    return 0.1952 * (T/100)**3 * E_s / (2 - E_s)
+
+
 def effective_thermal_conductivity(
-        T_f: float,
-        T_s: float,
         k_f: float,
         k_s: float,
-        void: float,
-        emiss: float,
+        eps: float,
+        h_rv: float,
+        h_rs: float,
+        phi: float,
         d: float,
         *,
         beta: float = 0.9,
@@ -21,47 +103,26 @@ def effective_thermal_conductivity(
     +\gamma\left(\frac{k_f}{k_s}\right)} \right]
     $$
 
-    The void-to-void and surface-to-surface radiative heat transfer coefficients are from Yagi and Kunii[^2].
-
     [^1]: D. Kunii and J. Smith, “Heat transfer characteristics of porous rocks,” AIChE Journal, vol. 6, no. 1,
     pp. 71–78, 1960.
-    [^2]: S. Yagi and D. Kunii, “Studies on effective thermal conductivities in packed beds,” AIChE Journal,
-    vol. 3, no. 3, pp. 373–381, 1957.
 
     Parameters:
-        T_f: Fluid temperature [K].
-        T_s: Solid temperature [K].
         k_f: Thermal conductivity of the fluid [W/mK].
         k_s: Thermal conductivity of the solid [W/mK].
-        void: Void fraction [-].
-        emiss: Emissivity of the solid [-].
+        eps: Void fraction [-].
+        h_rv: Void-to-void radiative heat transfer coefficient.
+        h_rs: Surface-to-surface radiative heat transfer coefficient.
+        phi: Effective film thickness ratio.
         d: Solid particle diameter [m].
         beta: Ratio of the average length between the centers of two neighboring solids to
             the mean particle diameter.
+
     """
-
-    gamma = 2 / 3
-
-    h_rv = 0.1952 * (T_f/100)**3 / (1 + void * (1 - emiss) / (2 * emiss * (1 - void)))
-    h_rs = 0.1952 * (T_s/100) ** 3 * emiss / (2 - emiss)
-
-    kappa = k_s / k_f
-
-    def phi_i(n_i):
-        cos_theta_i = (1 - 1/n_i)**0.5
-        return 1/2 * ((kappa - 1) / kappa)**2 / n_i / (
-            np.log(kappa - (kappa - 1) * cos_theta_i) - (kappa - 1) / kappa * (1 - cos_theta_i)
-        ) - 2 / (3 * kappa)
-
-    phi1 = phi_i(1.5)
-    phi2 = phi_i(4 * 3 ** 0.5)
-    phi = phi2 + (phi1 - phi2) * (void - 0.26) / (0.476 - 0.26)
-
     return k_f * (
-            void * (1 + beta * h_rv * d / k_f) +
-            beta * (1 - void) / (
-                    1 / (1 / phi + h_rs * d / k_f)
-                    + gamma * k_f / k_s)
+        eps * (1 + beta * h_rv * d / k_f) +
+        beta * (1 - eps) / (
+                1 / (1 / phi + h_rs * d / k_f)
+                + 2/3 * k_f / k_s)
     )
 
 
